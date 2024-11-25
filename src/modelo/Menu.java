14aa40a0d8ec66431fce2a992bcc540083b9a633
Dharla51 <dharlajduran@unicesar.edu.co>
2024-11-25 11:46:14 -0500

    menu

---
+++ src/modelo/Menu.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelo;
+
+import vista.viewMenu2;
+
+/**
+ *
+ * @author hp
+ */
+public class Menu {
+    public static void main(String[] args){
+        viewMenu2 menu= new viewMenu2(); 
+        menu.setVisible(true);
+    } 
+    
+}
